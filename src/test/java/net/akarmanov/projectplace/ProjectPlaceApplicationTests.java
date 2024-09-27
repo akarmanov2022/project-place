35268715e6e6f9afba446505230feb3dd7af38d4
akarmanov <mrmorris853@gmail.com>
2024-09-27 17:17:21 +0700

    Initial commit

---
+++ src/test/java/net/akarmanov/projectplace/ProjectPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.akarmanov.projectplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPlaceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
