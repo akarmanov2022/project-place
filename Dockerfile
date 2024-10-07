d6ebbd04d080dfa4b6fb03d2357baf6e7ddcb72b
akarmanov <mrmorris853@gmail.com>
2024-10-08 00:20:36 +0700

    Добавление Dockerfile и docker-compose.yaml

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM openjdk:21-jdk-slim
+
+WORKDIR /app
+
+COPY gradlew .
+COPY gradle gradle
+COPY build.gradle .
+COPY settings.gradle .
+
+COPY src src
+
+RUN chmod +x gradlew
+
+RUN ./gradlew build --no-daemon
+
+EXPOSE 8080
+
+CMD ["./gradlew", "bootRun"]
--- src/test/java/net/akarmanov/projectplace/api/UserRestControllerImplTest.java
+++ src/test/java/net/akarmanov/projectplace/api/UserRestControllerImplTest.java
@@ -1,28 +1,22 @@
 package net.akarmanov.projectplace.api;
 
+import net.akarmanov.projectplace.BaseApplicationTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.nio.charset.StandardCharsets;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
-class UserRestControllerImplTest {
+class UserRestControllerImplTest extends BaseApplicationTest {
 
     @Autowired
     private MockMvc mockMvc;
